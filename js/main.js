02403e4b2ee1f12c6aa7f79c44974a8bdddd65b6
abby-garcia <acg361@gmail.com>
2016-10-05 13:07:45 -0400

    it workkkssss

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
+var safeColors = ['00','33','66','99','cc','ff'];
+var rand = function() {
+    return Math.floor(Math.random()*6);
+};
+var randomColor = function() {
+    var r = safeColors[rand()];
+    var g = safeColors[rand()];
+    var b = safeColors[rand()];
+    return "#"+r+g+b;
+};
+
+
 
 $('button').on( "click", function() {
-  $("body").css('background', "red");
+  $("body").css('background', randomColor());
 });
